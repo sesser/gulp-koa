f425cced805a5c02a0ed0c5f805109c1fde019c7
Karina Pilyushonoka <k.pilyushonoka@gmail.com>
2015-10-03 23:36:19 +0300

    persisted koa server pid into file to be able kill it later

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ var through = require('through2'),
     spawn = require('child_process').spawn,
     debug = require('debug')('gulp-koa'),
     fs = require('fs'),
+    tmpdir = require('os-tmpdir')(),
+    mkdirp = require('mkdirp'),
     path = require('path'),
     info = gutil.colors.cyan,
     error = gutil.colors.bold.red,
@@ -12,12 +14,23 @@ var through = require('through2'),
     server = null;
 
 
-function stop() {
+function stop(script) {
     if (server && server.kill) {
         debug(info('Server is already running: ') + dbg(server.pid || 0));
         server.kill('SIGTERM');
         server = null;
+    } else if (!server) {
+        var pidPath = getPidPathFor(script);
+        if(fs.existsSync(pidPath)){
+            var pid = fs.readFileSync(pidPath, 'utf8') | 0; //read pid and convert it to int
+            console.log('send SIGTERM to pid ' + pid);
+            process.kill(pid);
+        };
     }
+}
+
+function getPidPathFor(script) {
+    return path.join(tmpdir, 'gulp-koa', path.dirname(script), path.basename(script) + ".pid");
 }
 
 module.exports = function (script, options) {
@@ -38,11 +51,16 @@ module.exports = function (script, options) {
         stdio: 'inherit'
     }, options);
     return through.obj(function () {
-        stop();
-
+        stop(script);
         debug(info('Spawning new server with: ' + script));
-
         server = spawn('node', ['--harmony', script], opts);
+        var pidPath = getPidPathFor(script);
+        fs.writeFileSync(pidPath, server.pid.toString());
+        console.log('write pid ' + server.pid + ' to ' + pidPath);
+        server.on('exit', function(code){
+            console.log('Koa has stopped with exit code ' + code);
+            fs.unlinkSync(pidPath);
+        });
     });
 }
 
